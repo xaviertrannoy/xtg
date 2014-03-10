8d6436e6b0d9e0a1b0f56ffc696a3f7cf8f38822
Xavier Trannoy <xavier@xtg.fr>
2014-03-10 19:18:49 +0100

    amelioration du code

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Xtg::Application.routes.draw do
 
-  get "main/xtg"
-  get "main/projet"
-  get "main/motul"
-  get "main/ppr"
+  match 'projets/xtg', to: 'main#xtg', via: :get, as: 'xtg'
+  match 'projets/projet', to: 'main#projet', via: :get, as: 'projet'
+  match 'projets/motul', to: 'main#motul', via: :get, as: 'motul'
+  match 'projets/ppr', to: 'main#ppr', via: :get, as: 'ppr'
 
   match 'projets/histoire-de-france', to: 'main#histoire-de-france', via: :get, as: 'histoire_de_france'
 
