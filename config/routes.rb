b4f5450b030693408b66567f866ce66be8ef4703
Xavier Trannoy <xavier@xtg.fr>
2014-03-11 14:35:47 +0100

    création de la page PHB Conseil

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Xtg::Application.routes.draw do
   match 'projets/projet', to: 'main#projet', via: :get, as: 'projet'
   match 'projets/motul', to: 'main#motul', via: :get, as: 'motul'
   match 'projets/ppr', to: 'main#ppr', via: :get, as: 'ppr'
-
   match 'projets/histoire-de-france', to: 'main#histoire-de-france', via: :get, as: 'histoire_de_france'
-
+  match 'projets/phb-conseil', to: 'main#phb-conseil', via: :get, as: 'phb_conseil'
 
 end
